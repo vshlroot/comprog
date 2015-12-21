cec06eb0494cb2823434471bf4f14200e6c0c7fd
Vshlroot <vshlroot@gmail.com>
2015-12-21 21:52:25 +0530

    Create LeetCode283.java

---
+++ leetcode/LeetCode283.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * Created by vishalss on 12/21/2015.
+ */
+/*
+ Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+Note:
+
+    You must do this in-place without making a copy of the array.
+    Minimize the total number of operations.
+
+ */
+
+public class LeetCode283 {
+    public void moveZeroes(int[] nums) {
+        int i=0,j=0;
+        for(i=0,j=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        while(j<nums.length){
+            nums[j]=0;
+            j++;
+        }
+    }
+}
